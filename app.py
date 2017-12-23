9a106a302f06f75ee5e9ce9eb9f460b384b34614
blgo <jblas1989@gmail.com>
2017-12-23 14:51:28 +0000

    RESTful api moved to structured folder

--- app.py
+++ app.py
@@ -1,39 +1,19 @@
 from flask import Flask, render_template, request
-from flask_restful import Resource, Api, reqparse, inputs
+from flask_restful import Api
 from flask_socketio import SocketIO, emit
 from threading import Lock
 import eventlet
 
-thread = None
-thread_lock = Lock()
+from realtimeapp.resources.restfulapi import TodoSimple
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "verysecret"
 
-#RESTful api
 
+# RESTful api
+# Add data to the server using: curl http://localhost:5000/todo1 -d "data=it works!" -X PUT
 api = Api(app)
-parser = reqparse.RequestParser()
-# parser.add_argument('data',type=inputs.regex('^\D+$') , help="data has to be a string")
-parser.add_argument('data',type=int , help="data has to be a number")
-
-todos = {}
-
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id:todos[todo_id]}
-
-    def put(self, todo_id):
-        args = parser.parse_args(strict=True)
-        todos[todo_id] = request.form['data']
-        
-        # use websockets in background when new data is created
-        global thread
-        with thread_lock:
-            thread = socketio.start_background_task(target=background_thread)
-        
-        return {todo_id:todos[todo_id]}
-
 api.add_resource(TodoSimple, '/<string:todo_id>')
 
 
@@ -42,12 +22,19 @@ async_mode = 'eventlet'
 socketio = SocketIO(app, async_mode=async_mode)
 
 
+thread = None
+thread_lock = Lock()
+
+        # # use websockets in background when new data is created
+        # global thread
+        # with thread_lock:
+        #     thread = socketio.start_background_task(target=background_thread)
 
 
 def background_thread():
     """Example of how to send server generated events to clients."""
     count = 0
-    #Add data to the server using: curl http://localhost:5000/todo1 -d "data=it works!" -X PUT
+
     if todos.get("todo1"):
         socketio.emit('my_restful_data',
                     {'data': todos["todo1"], 'count': count},
