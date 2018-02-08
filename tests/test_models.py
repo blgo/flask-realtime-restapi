1b7a50f9d154340f00e01ebb0d263823a8e96bc0
blgo <blgo@users.noreply.github.com>
2018-02-08 17:02:03 +0000

    Add test models

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,7 +5,7 @@ from nose.tools import assert_equal, assert_raises
 def test_sensor_reading_save():
     '''
     Test models using MongoDB
-    Create and Read data
+    Create and Read a new datapoint 
     A Mongo database should be running to test this:
     $ docker run --rm -p 27017:27017 -d mongo:3.6.2
     '''
@@ -15,7 +15,8 @@ def test_sensor_reading_save():
         room='backyard_test',
         temperature=15,
         humidity=99,
-        date='2018-01-05T15:48:11.893728+00:00'
+        date='2018-01-05T15:48:11.893728+00:00',
+        readingid = 'backyard_test_1201801051548'
     )
     reading_1.save()       # This will perform an insert
 
@@ -23,7 +24,8 @@ def test_sensor_reading_save():
         room='backyard_test_2',
         temperature=11.1,
         humidity=51.5,
-        date='2018-01-05T16:50:21.114721+00:00'
+        date='2018-01-05T16:50:21.114721+00:00',
+        readingid = 'backyard_test_220180105165021'
     )
     reading_2.save()
 
@@ -43,13 +45,14 @@ def test_sensor_reading_validation_error():
     Test validation, validation should be triggered and save command fail
     '''
     reading_3 = SensorReading(
-        room='backyard_test_2',
+        room='backyard_test_3',
         temperature='string',
         humidity=51.5,
-        date='2018-01-05T16:50:21.114721+00:00'
+        date='2018-01-05T16:50:21.114721+00:00',
+        readingid = 'backyard_test_3201801051650'
     )
     
     with assert_raises(Exception) as e:
         reading_3.save()
 
-    assert_equal(e.exception._message,'ValidationError (SensorReading:None) ') 
+    assert_equal(e.exception._message,'ValidationError (SensorReading:backyard_test_3201801051650) ') 
