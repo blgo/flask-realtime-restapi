dfebb8b192267d108d42d8fdc95af61095830757
blgo <blgo@users.noreply.github.com>
2018-02-09 18:19:15 +0000

    Amend docker containers networking

--- main.py
+++ main.py
@@ -12,16 +12,19 @@ create_api(app)
 # Initialise flask-socketio
 create_socketio(app)
 
+if not environ.get('FLASK_DEBUG', None): 
+    mongodb_location = environ.get('MONGODB_NETWORK_ALIAS', "localhost")
+    connect('mongoengine', host=mongodb_location, port=27017)
+    print('Connected to MongoDB instance in ', mongodb_location)
+    print('If you are testing and need a mock database use: $ export FLASK_DEBUG=1')
+else:
+    connect('mongoengine_test', is_mock=True)
+    print('Connected to mock database.')
+    
+
+
 if __name__ == '__main__':
     # the start point is socketio.run() uses eventlet which provides a production ready 
     # Establishing a Connection
-    if not environ.get('FLASK_DEBUG', None): 
-        connect('mongoengine', host='localhost', port=27017)
-        print('Connected to MongoDB instance in localhost')
-        print('If you are testing and need a mock database use: $ export FLASK_DEBUG=1')
-    else:
-        connect('mongoengine_test', is_mock=True)
-        print('Connected to mock database.')
-        
 
     socketio.run(app)
