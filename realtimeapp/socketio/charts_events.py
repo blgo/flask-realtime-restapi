4c9b8a9bca171e302f6691c8263355d8ca3e502c
blgo <blgo@users.noreply.github.com>
2018-02-13 17:27:12 +0000

    Show only last 24h for charts

--- realtimeapp/socketio/charts_events.py
+++ realtimeapp/socketio/charts_events.py
@@ -3,7 +3,11 @@ from flask_socketio import emit
 from threading import Lock
 from . import socketio
 from ..readingstats import readings_to_matrix, generate_stats, transpose_readings
+<<<<<<< HEAD
 from ..models import return_all, last_reading
+=======
+from ..models import SensorReading, return_all_by_date
+>>>>>>> Show only last 24h for charts
 
 from random import randint
 import datetime
@@ -35,7 +39,7 @@ def get_chart_data():
 
 
 
-    THERMOHYGRO = return_all()
+    THERMOHYGRO = return_all_by_date(days=1)
     if THERMOHYGRO:
         readings_matrix = readings_to_matrix(THERMOHYGRO)
         transposed_readings = transpose_readings(readings_matrix)
@@ -57,10 +61,10 @@ def get_chart_data():
 
 def emit_new_reading(reading):
     
-    THERMOHYGRO = return_all()
-
-    socketio.emit('last_reading', {'label': reading['date'] , 'dataa':  reading['temperature'], 'datab':  reading['humidity'] }, namespace='/charts')
-    socketio.emit('my_chart', {'label': reading['date'] , 'dataa':  reading['temperature'], 'datab':  reading['humidity'] }, namespace='/charts')
-    stats = generate_stats(THERMOHYGRO)
-    socketio.emit('my_chart_stats', stats, namespace='/charts')
+    THERMOHYGRO = return_all_by_date(days=1)
+    if THERMOHYGRO:
+        socketio.emit('my_chart', {'label': reading['date'] , 'dataa':  reading['temperature'], 'datab':  reading['humidity'] }, namespace='/charts')
+        stats = generate_stats(THERMOHYGRO)
+        socketio.emit('my_chart_stats', stats, namespace='/charts')
+    
     socketio.emit('my_response', {'data': 'Data received from sensor', 'count': 0}, namespace="/charts")
